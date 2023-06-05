6b07d432825ecfadf2af5bb0e750d659caa67993
Mike-Skehan <mskeha01@student.bbk.ac.uk>
2023-06-06 00:11:24 +0100

    RNN work, modified gbt function to include pred vs act

--- RNN.py
+++ RNN.py
@@ -1,13 +1,28 @@
 import numpy as np
+from scipy.stats import pearsonr
 from sklearn.metrics import mean_absolute_error, r2_score
+import seaborn as sns
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
+import pandas as pd
 
 
 def rnn_cv(X,y, num_epochs=100, batch_size=10):
-    # set up the subplots
+
+    sns.set_style(style='white')
+
+    rand = 28
+
+    y = pd.DataFrame(y)
+    y = y.values.ravel()
+
+    X.columns = ['{}'.format(i) for i in range(len(X.columns))]
+
+    # set up train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=rand)
+
     fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(10, 15))
     axs = axs.flatten()
 
@@ -18,23 +33,24 @@ def rnn_cv(X,y, num_epochs=100, batch_size=10):
     # set up the k-fold cross-validation
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-    for i, (train_index, test_index) in enumerate(kf.split(X)):
+    for i, (train_index, val_index) in enumerate(kf.split(X_train)):
 
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        k_X_train, X_val = X_train.iloc[train_index], X_train.iloc[val_index]
+        k_y_train, y_val = y_train[train_index], y_train[val_index]
 
         # create the RNN model
-        X_train = X_train.values.reshape((X_train.shape[0], X_train.shape[1], 1))
-        X_test = X_test.values.reshape((X_test.shape[0], X_test.shape[1], 1))
-        y_train = np.array(y_train)
-        y_test = np.array(y_test)
+        k_X_train = k_X_train.values.reshape((k_X_train.shape[0], k_X_train.shape[1], 1))
+        X_val = X_val.values.reshape((X_val.shape[0], X_val.shape[1], 1))
+        k_y_train = np.array(k_y_train)
+        y_val = np.array(y_val)
         model = Sequential()
-        model.add(LSTM(100, input_shape=(X_train.shape[1], X_train.shape[2])))
+        model.add(LSTM(100, input_shape=(k_X_train.shape[1], k_X_train.shape[2])))
         model.add(Dense(1))
         model.compile(loss='mean_absolute_error', optimizer='adam')
 
         # fit the model
-        history = model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=2, validation_data=(X_test, y_test))
+        history = model.fit(k_X_train, k_y_train, epochs=num_epochs, batch_size=batch_size, verbose=2, validation_data=
+        (X_val, y_val))
 
         # evaluate the model on the test set
         predictions = model.predict(X_test)
@@ -67,6 +83,14 @@ def rnn_cv(X,y, num_epochs=100, batch_size=10):
     print('MAE: %.3f (%.3f)' % (np.mean(mae_scores), np.std(mae_scores)))
     print('R2: %.3f (%.3f)' % (np.mean(r2_scores), np.std(r2_scores)))
     print('Pearsonr: %.3f (%.3f)' % (np.mean(pearsonr_scores), np.std(pearsonr_scores)))
+
+    plt.figure(figsize=(10, 10))
+    plt.scatter(y_test, predictions)
+    plt.title(f'Predictions vs. Actual\nMAE={(np.mean(mae_scores)):.2f}, R2={(np.mean(r2_scores)):.2f}, Pearsonr={(np.mean(pearsonr_scores)):.2f}')
+    plt.ylabel('Actual')
+    plt.xlabel('Predictions')
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2)
+    plt.show()
 
     return model
 
--- gradient_boosting.py
+++ gradient_boosting.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, ensemble
-from sklearn.inspection import permutation_importance
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import seaborn as sns
+from scipy.stats import stats
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from matplotlib import pyplot as plt
 
 
 def gradient_boosting_regression(X, y):
@@ -22,7 +22,7 @@ def gradient_boosting_regression(X, y):
     reg : regression model.
     """
     params = {
-        "n_estimators": 500,
+        "n_estimators": 5000,
         "max_depth": 4,
         "min_samples_split": 5,
         "learning_rate": 0.01,
@@ -38,11 +38,18 @@ def gradient_boosting_regression(X, y):
     reg.fit(X_train, y_train)
 
     mse = mean_squared_error(y_test, reg.predict(X_test))
+    mae = mean_absolute_error(y_test, reg.predict(X_test))
+    pearsonr = stats.pearsonr(y_test, reg.predict(X_test))
+    r2 = r2_score(y_test, reg.predict(X_test))
+
     print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
+    print("The mean absolute error (MAE) on test set: {:.4f}".format(mae))
+    print("The pearson coeffieicent on test set: {:.4f}".format(pearsonr[0]))
+    print("The r2 on test set: {:.4f}".format(r2))
 
     test_score = np.zeros((params["n_estimators"],), dtype=np.float64)
     for i, y_pred in enumerate(reg.staged_predict(X_test)):
-        test_score[i] = mean_squared_error(y_test, y_pred)
+        test_score[i] = mean_absolute_error(y_test, y_pred)
 
     fig = plt.figure(figsize=(6, 6))
     plt.subplot(1, 1, 1)
@@ -51,14 +58,22 @@ def gradient_boosting_regression(X, y):
         np.arange(params["n_estimators"]) + 1,
         reg.train_score_,
         "b-",
-        label="Training Set Deviance",
-        )
-    plt.plot(
-        np.arange(params["n_estimators"]) + 1, test_score, "r-", label="Test Set Deviance"
+        label="Training Set Deviance"
     )
     plt.legend(loc="upper right")
     plt.xlabel("Boosting Iterations")
     plt.ylabel("Deviance")
+    fig.tight_layout()
+    plt.show()
+
+    # plot actual vs predicted
+    fig = plt.figure(figsize=(6, 6))
+    plt.subplot(1, 1, 1)
+    plt.scatter(reg.predict(X_test),y_test, alpha=0.5)
+    plt.plot([60,80], [60,80], 'r--', lw=2)
+    plt.ylabel("Actual")
+    plt.xlabel("Predicted")
+    plt.title(f'Predictions vs. Actual\nMAE={mae:.2f}, R2={r2:.2f}, Pearsonr={pearsonr[0]:.2f}')
     fig.tight_layout()
     plt.show()
 
